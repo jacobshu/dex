ada18a758df83152867ae745d17602f9e0f3d263
Jacob Shulenberger <jacob@builtdigital.io>
2025-01-22 14:22:42 -0600

    feat: basic repl

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			input := scanner.Text()
+			sanitized := cleanInput(input)
+			if len(sanitized) == 0 {
+				continue
+			}
+			cmd := sanitized[0]
+			if cmd == "quit" || cmd == "q" {
+				fmt.Print("bye 👋\n")
+				return
+			}
+			fmt.Printf("Your command was: %s\n", cmd)
+		}
+	}
 }
 
 func cleanInput(text string) []string {
-	return strings.Fields(text)
+	return strings.Fields(strings.ToLower(text))
 }
